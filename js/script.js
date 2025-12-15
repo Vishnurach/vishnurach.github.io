44ffc529004ddc0098e0115db533a19f7b9db78f
Vishnurach <vishnurach@gmail.com>
2025-12-15 21:55:51 +0530

    Single page website

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('navMenu');
-
-    hamburger.addEventListener('click', () => {
-        navMenu.classList.toggle('active');
-        hamburger.classList.toggle('active');
-    });
-});
